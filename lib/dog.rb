33a05847a16764131a516cd3647df4d97552a213
Kareem Rolle <kareemrolle@yahoo.com>
2020-01-30 17:40:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,82 +1,72 @@
 # Add your code here
-class Owner
-OWNERS = [] 
-attr_accessor :name, :pets 
-attr_reader :species 
+class Shelter  
+  attr_accessor :name, :city
+  @@all = []
 
-def initialize(species)
-  @species = species 
-  OWNERS << self 
-  @pets = {:fishes => [], :dogs => [], :cats => []}
-end 
-
-def self.all ##Do we have to use this if we use a class variable? @@all 
-OWNERS 
-end 
-
-def self.reset_all 
-OWNERS.clear 
-end 
-
-def self.count
-OWNERS.size 
-end
-
-def species 
-  @species 
-end 
-
-def say_species 
-  return "I am a #{species}"
-end 
+  def initialize (name, city)
+    @name = name
+    @city = city
+    @dogs = [] 
+    @@all << self 
+  end 
   
-def name 
-  @name 
-end 
-
-def buy_fish(name)
-  pets[:fishes] << Fish.new(name)
-end 
-
-def buy_cat(name)
-  pets[:cats] << Cat.new(name)
-end 
-
-def buy_dog(name)
-  pets[:dogs] << Dog.new(name)
-end 
-
-def walk_dogs
-  pets[:dog].each do |dog|
-    dog.mood = "happy"
+  def add_dog(dog)
+    dog.shelter = self
+    @dogs << dog 
+  end 
+  
+  def dogs 
+    @dogs 
+  end 
+  
+  def self.all 
+    @@all
   end 
 end 
 
-def play_with_cats
-  pets[:cats].each do |cat| 
-  cat.mood = "Happy"
+class Dog  
+  attr_accessor :name, :age, :breed, :shelter, :owner
+  @@all = []
+  
+  def initialize(name, age, breed)
+    @name = name
+    @age = age
+    @breed = breed 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
   end 
 end 
 
-def feed_fish
-  pets[:fish].each do |fish|
-  fish.mood = "happy"
+
+class Owner # has many dogs 
+  attr_accessor :name, :age
+  attr_reader :dogs 
+  @@all = []
+  
+   def initialize (name, age)
+    @name = name
+    @age = age
+    @dogs = []
+    @shelters = []
+    @@all << self 
   end 
-end 
-
-def list_pets
-  "I have #{pets[:fishes].count} fish. I have #{pets[:cats].count} cats. I have #{pets[:dogs].count} dogs."
-end 
-
-end 
-
-class Fish 
-attr_reader :name 
-attr_accessor :mood 
-
-def initialize 
-  @name = name 
-  @mood = "nervous"
-end 
-
-end 
+  
+  def add_dog(dog)
+    @dogs << dog 
+    dog.owner = self 
+  end 
+  
+  def shelters
+    self.dogs.each do |dog| 
+      @shelters << dog.shelter if dog.shelter
+    end 
+    @shelters
+  end
+  
+  def self.all 
+    @@all
+  end 
+end
